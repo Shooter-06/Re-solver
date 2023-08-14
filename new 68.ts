726c8c6ca371ec57f62a828cbfa147f18ed950f5
Fiacre YIRB <john190curry@gmail.com>
2023-08-14 11:48:47 -0400

    new

--- new 68.ts
+++ new 68.ts
@@ -16,3 +16,36 @@ export const siteResolver: Resolve = (route: ActivatedRouteSnapshot, state: Rout
 
   return service.get(id);
 };
+
+
+import { Injectable, Inject } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { PortalSites, PortalSiteService } from './path-to-your-service'; // Please update the import path
+
+type Data = any; // Define your Data type if required
+
+export declare type SiteResolvedData = Data & { site?: PortalSites };
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SiteResolver implements Resolve<SiteResolvedData> {
+  constructor(@Inject(PortalSiteService) private service: PortalSiteService) {}
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<SiteResolvedData> | Promise<SiteResolvedData> | SiteResolvedData {
+    const idParam = route.paramMap.get('id');
+    let id: number;
+
+    if (idParam) {
+      id = parseInt(idParam, 10);
+      if (isNaN(id) || !isFinite(id)) {
+        return of(undefined);
+      }
+    } else {
+      return undefined;
+    }
+
+    return this.service.get(id);
+  }
+}
