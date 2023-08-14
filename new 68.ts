74ae2b1e8dfa81c32d0b1ebcab333c1232713502
Fiacre YIRB <john190curry@gmail.com>
2023-08-14 11:52:26 -0400

    new

--- new 68.ts
+++ new 68.ts
@@ -1,51 +1,82 @@
-export declare type SiteResolvedData = Data & { site?: PortalSites };
+// export declare type SiteResolvedData = Data & { site?: PortalSites };
 
-export const siteResolver: Resolve = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-  const service = inject(PortalSiteService);
-  const idParam = route.paramMap.get('id');
-  let id: number;
+// export const siteResolver: Resolve = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+//   const service = inject(PortalSiteService);
+//   const idParam = route.paramMap.get('id');
+//   let id: number;
 
-  if (idParam) {
-    id = parseInt(idParam, 10);
-    if (isNaN(id) || !isFinite(id)) {
-      return of(undefined);
-    }
-  } else {
-    return undefined;
-  }
+//   if (idParam) {
+//     id = parseInt(idParam, 10);
+//     if (isNaN(id) || !isFinite(id)) {
+//       return of(undefined);
+//     }
+//   } else {
+//     return undefined;
+//   }
 
-  return service.get(id);
-};
+//   return service.get(id);
+// };
 
 
-import { Injectable, Inject } from '@angular/core';
+// import { Injectable, Inject } from '@angular/core';
+// import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+// import { Observable, of } from 'rxjs';
+// import { PortalSites, PortalSiteService } from './path-to-your-service'; // Please update the import path
+
+// type Data = any; // Define your Data type if required
+
+// export declare type SiteResolvedData = Data & { site?: PortalSites };
+
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class SiteResolver implements Resolve<SiteResolvedData> {
+//   constructor(@Inject(PortalSiteService) private service: PortalSiteService) {}
+
+//   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<SiteResolvedData> | Promise<SiteResolvedData> | SiteResolvedData {
+//     const idParam = route.paramMap.get('id');
+//     let id: number;
+
+//     if (idParam) {
+//       id = parseInt(idParam, 10);
+//       if (isNaN(id) || !isFinite(id)) {
+//         return of(undefined);
+//       }
+//     } else {
+//       return undefined;
+//     }
+
+//     return this.service.get(id);
+//   }
+// }
+
+
+import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable, of } from 'rxjs';
-import { PortalSites, PortalSiteService } from './path-to-your-service'; // Please update the import path
-
-type Data = any; // Define your Data type if required
-
-export declare type SiteResolvedData = Data & { site?: PortalSites };
+import { map } from 'rxjs/operators';
+import { PortalSiteService } from './portal-site.service'; // Import path should be adjusted based on your project structure
+import { SiteResolvedData } from './site-resolved-data.model'; // Import path should be adjusted based on your project structure
 
 @Injectable({
   providedIn: 'root',
 })
 export class SiteResolver implements Resolve<SiteResolvedData> {
-  constructor(@Inject(PortalSiteService) private service: PortalSiteService) {}
+  constructor(private service: PortalSiteService) {}
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<SiteResolvedData> | Promise<SiteResolvedData> | SiteResolvedData {
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<SiteResolvedData> {
     const idParam = route.paramMap.get('id');
     let id: number;
 
     if (idParam) {
       id = parseInt(idParam, 10);
       if (isNaN(id) || !isFinite(id)) {
-        return of(undefined);
+        return of({ site: undefined });
       }
     } else {
-      return undefined;
+      return of({ site: undefined });
     }
 
-    return this.service.get(id);
+    return this.service.get(id).pipe(map(site => ({ site })));
   }
 }
